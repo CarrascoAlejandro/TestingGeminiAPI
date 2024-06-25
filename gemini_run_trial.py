bac6092721f058d90c8dedc8806eb038174bcaea
Alejandro Carrasco <carrascoalejandro2003@gmail.com>
2024-06-25 16:06:45 -0400

    fixing exposure of API key

--- gemini_run_trial.py
+++ gemini_run_trial.py
@@ -1,7 +1,7 @@
 import google.generativeai as genai
 import os
 
-genai.configure(api_key="AIzaSyDZjGnfnrbrMwcSiw0yojUvVTWQP7DHqcE")
+genai.configure(api_key=os.environ.get("GEMINI_API_KEY"))
 
 model = genai.GenerativeModel('gemini-1.5-flash')
 
